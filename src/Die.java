2a19f23c1621c356a462d75906a8d884a71247e5
aleksanderlj <aleksanderlj@hotmail.com>
2018-10-08 15:43:36 +0200

    Metodeliste i Die klasse lavet

--- src/Die.java
+++ src/Die.java
@@ -1,2 +1,10 @@
 public class Die {
 }
+
+
+//Rul - få et tal fra 1-6 (samt tilføj faceValue til Player objekt)
+//Getters og setters
+
+
+//Ekstra!
+//Sammenlign - Sammenlign to Die-objekters face-value (hint: this.x == other.x)
