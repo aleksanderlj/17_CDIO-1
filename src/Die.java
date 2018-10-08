40571a2d47259eb7e567fdf17b27310ab813e043
Andreas <mail.Tailspot@gmail.com>
2018-10-08 16:14:40 +0200

    Rul metode lavet

--- src/Die.java
+++ src/Die.java
@@ -1,6 +1,20 @@
-public class Die {
-}
+import java.util.Random;
 
+public class Die {
+private int faceValue;
+
+
+
+    public void rul() {
+        Random r = new Random();
+        this.faceValue = r.nextInt(6) + 1;
+    }
+
+    public int getFaceValue(){
+        return this.faceValue;
+    }
+
+}
 
 //Rul - få et tal fra 1-6 (samt tilføj faceValue til Player objekt)
 //Getters og setters
