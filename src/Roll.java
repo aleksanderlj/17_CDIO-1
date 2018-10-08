0bce33730f44752d02f3cfc37b6262d85d2a7968
aleksanderlj <aleksanderlj@hotmail.com>
2018-10-08 17:25:31 +0200

    Grundspil lavet

--- src/Roll.java
+++ src/Roll.java
@@ -13,7 +13,43 @@ public class Roll {
         Player p1 = new Player(name1);
         Player p2 = new Player(name2);
 
+        Die d1 = new Die();
+        Die d2 = new Die();
 
+        int round=1;
+        do{
+            System.out.println("---------------------------------");
+            System.out.println("Runde " + round);
+            System.out.println("---------------------------------");
 
+            System.out.println(p1.getName() + " rul");
+            input.nextLine();
+            d1.rul();
+            d2.rul();
+
+            p1.setScore((p1.getScore() + d1.getFaceValue() + d2.getFaceValue()));
+            System.out.println("Du rullede " + d1.getFaceValue() + " og " + d2.getFaceValue());
+            System.out.println(p1.getName() + ", din score er nu " + p1.getScore());
+
+            System.out.println();
+            System.out.println(p2.getName() + " rul");
+            input.nextLine();
+            d1.rul();
+            d2.rul();
+
+            p2.setScore((p2.getScore() + d1.getFaceValue() + d2.getFaceValue()));
+            System.out.println("Du rullede " + d1.getFaceValue() + " og " + d2.getFaceValue());
+            System.out.println(p2.getName() + ", din score er nu " + p2.getScore());
+            System.out.println();
+
+            round++;
+        }while ((p1.getScore() <= 40) && (p2.getScore() <= 40));
+
+        if (p2.getScore() < 40)
+            System.out.println(p1.getName() + " har vundet!");
+        else if (p1.getScore() < 40)
+            System.out.println(p2.getName() + " har vundet!");
+        else
+            System.out.println("I har begge vundet!");
     }
 }
