d6c494bb4446d8751236b7492111483c4b669d31
aleksanderlj <aleksanderlj@hotmail.com>
2018-10-09 17:02:40 +0200

    Player.turn lavet. Andre små ændringer tilføjet (inklusiv kommentarer)

--- src/spil/Player.java
+++ src/spil/Player.java
@@ -4,11 +4,20 @@ public class Player {
     private int score = 0;
     String name;
 
-    //-----------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
     // Constructor til spil.Player. Når objektet laves gives en String som er navnet til spilleren.
-    //-----------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
     public Player(String name){
         this.name = name;
+    }
+
+    //--------------------------
+    //Spiller ruller terninger
+    //--------------------------
+    public void turn(Die d1, Die d2){
+        d1.rul();
+        d2.rul();
+        this.setScore((this.getScore() + d1.getFaceValue() + d2.getFaceValue()));
     }
 
     //--------------------
